9732c476586aef543c17ea0e2737c63ba410e41c
Gavin <111817327+gavinwebst@users.noreply.github.com>
2024-09-24 09:10:52 -0400

    week5a class work

---
+++ Week5a/Week5a/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Week5a
+{
+    internal class Program
+    {
+        static void InstructorName() {
+            string instructorName = "Nick";
+            Console.WriteLine(instructorName);
+        }
+        static void Main(string[] args)
+        {
+            int number = 100;
+            number += 12;
+            Console.WriteLine(number);
+            string myName = "Gavin W.";
+            Console.WriteLine(myName);
+            bool myBool = false;
+            Console.WriteLine(myBool);
+            InstructorName();
+        }
+    }
+}
